03aceb50d9c01972bca576f0cd94a8b302cdbec5
Tim Martin <tim@timmart.in>
2021-08-08 23:38:48 -0400

    Redirect to home page when year/month not in data

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,7 +1,26 @@
+import { useParams, useHistory, Redirect } from "react-router-dom";
+import { useContext } from 'preact/hooks';
+
+import { IndexRouteParams } from "./IndexRouteParams";
 import { Month } from './Month';
 import { DateSelector } from './DateSelector';
+import { PostsContext } from './PostsContext';
 
 export function Index() {
+  const params = useParams<IndexRouteParams>();
+
+  const month = params.month;
+  const year = params.year;
+
+  const posts = useContext(PostsContext);
+
+  if (posts[year] === undefined || posts[year][month] === undefined) {
+    const history = useHistory();
+    return (
+      <Redirect to="/" />
+    )
+  }
+
   return (
     <>
       <DateSelector />
