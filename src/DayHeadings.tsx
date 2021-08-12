b41c4c9d0af3924702541d928cdba22ec07a41a2
Tim Martin <tim@timmart.in>
2021-08-12 00:19:38 -0400

    Abbreviate day of week for small screens

--- src/DayHeadings.tsx
+++ src/DayHeadings.tsx
@@ -1,9 +1,14 @@
 export function DayHeadings() {
   const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const dayHeadings = daysOfWeek.map((dayName) => (
-    <span>
-      {dayName}
-    </span>
+    <div>
+      <span class="md:block hidden">
+        {dayName}
+      </span>
+      <span class="md:hidden block">
+        {dayName.substring(0, 1)}
+      </span>
+    </div>
   ))
 
   return (
